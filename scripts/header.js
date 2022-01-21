0cbca3d7cf72c51894e37bc220a874cc29329dfe
yummyHouse <96030045+yummyHouse@users.noreply.github.com>
2022-01-21 18:09:15 +0800

    2022.0121ver

---
+++ scripts/header.js
@@ -0,0 +1,13 @@
+header =`<nav class="header">
+<ul class="menu">
+<li><a href="list.html">總表</a></li>
+<li><a href="index.html">基礎樣式</a></li>
+<li><a href="table.html">表格樣式</a></li>
+<li><a href="block.html">區塊樣式</a></li>
+</ul>
+<p id="show">
+你複製了什麼?
+</p>
+</nav>
+`;
+document.write(header);
