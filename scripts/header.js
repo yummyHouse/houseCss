16c931b286e391961b3d9245af2c46d199bf9fda
yummyHouse <96030045+yummyHouse@users.noreply.github.com>
2022-01-22 10:25:04 +0800

    20220122ver.2.1
    補齊總表

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 header =`<nav class="header">
 <ul class="menu">
-<li><a href="list.html">總表</a></li>
-<li><a href="index.html">基礎樣式</a></li>
+<li><a href="index.html">總表</a></li>
+<li><a href="basic.html">基礎樣式</a></li>
 <li><a href="table.html">表格樣式</a></li>
 <li><a href="block.html">區塊樣式</a></li>
 </ul>
